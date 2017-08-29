67a8d42790101e382b1bd459a696e8fa919a9508
4000D <dkdkajej@gmail.com>
2017-08-29 11:42:37 +0900

    truffle with ropsten network

--- truffle.js
+++ truffle.js
@@ -1,13 +1,49 @@
 require("babel-register");
 require("babel-polyfill");
 
+// http://truffleframework.com/tutorials/using-infura-custom-provider
+// const bip39 = require("bip39");
+// const hdkey = require("ethereumjs-wallet/hdkey");
+// const ProviderEngine = require("web3-provider-engine");
+// const WalletSubprovider = require("web3-provider-engine/subproviders/wallet.js");
+// const Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
+// const Web3 = require("web3");
+//
+// // Get our mnemonic and create an hdwallet
+// const mnemonic = process.env.MNEMONIC || "price legal churn escape digital timber menu replace crime interest great oblige";
+// const hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
+//
+// const hdPath = "m/44'/60'/0'/0/";
+// const wallet = hdwallet.derivePath(`${ hdPath }0`).getWallet();
+// const address = `0x${ wallet.getAddress().toString("hex") }`;
+//
+// const providerUrl = "https://ropsten.infura.io";
+// const engine = new ProviderEngine();
+// engine.addProvider(new WalletSubprovider(wallet, {}));
+// engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(providerUrl)));
+// engine.start(); // Required by the provider engine.
+
+// SNT
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const mnemonic = process.env.MNEMONIC || "price legal churn escape digital timber menu replace crime interest great oblige";
+const providerUrl = "https://ropsten.infura.io";
+
+const providerRopsten = new HDWalletProvider(mnemonic, providerUrl, 0);
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*",
-      gas: 1000000000,
+      gas: 4000000,
+    },
+    ropsten: {
+      network_id: 3,
+      provider: providerRopsten,
+      gas: 4500000,
+      // gasPrice: 20e9,
     },
     rinkeby: {
       host: "192.168.1.245", // go-ethereum conencted to rinkeby network is running on 192.168.1.245
