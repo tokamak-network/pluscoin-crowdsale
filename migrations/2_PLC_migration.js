a082c49482814e4a64d737c19c72c468b630b1fc
4000D <dkdkajej@gmail.com>
2017-08-29 11:41:22 +0900

    hard-coded reserve wallet address

--- migrations/2_PLC_migration.js
+++ migrations/2_PLC_migration.js
@@ -8,6 +8,9 @@ const RefundVault = artifacts.require("crowdsale/RefundVault.sol");
 const MultiSig = artifacts.require("wallet/MultiSigWallet.sol");
 
 module.exports = async function (deployer, network, accounts) {
+  console.log("[accounts]");
+  accounts.forEach((account, i) => console.log(`[${ i }]  ${ account }`));
+
   try {
     // const maxEtherCap = 100000 * 10 ** 18;
     // const minEtherCap = 30000 * 10 ** 18;
@@ -25,7 +28,13 @@ module.exports = async function (deployer, network, accounts) {
     const maxEtherCap = 5 * 10 ** 18;
     const minEtherCap = 1 * 10 ** 18;
 
-    const reserveWallet = accounts.slice(3, 3 + 5);
+    const reserveWallet = [
+      "0x922aa0d0e720caf10bcd7a02be187635a6f36ab0",
+      "0x6267901dbb0055e12ea895fc768b68486d57dcf8",
+      "0x236df55249ac7a6dfea613cd69ccd014c3cb8445",
+      "0xceca4d86a45cfef2e6431b4a871123a23bef6d87",
+      "0x8afe4672155b070e0645c0c9fc50d8eb3eab9a7e",
+    ];
 
     const multiSig = await MultiSig.new(reserveWallet, reserveWallet.length - 1); // 4 out of 5
     console.log("multiSig deployed at", multiSig.address);
