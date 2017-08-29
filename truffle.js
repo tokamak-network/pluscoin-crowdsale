c742c7009d21553a198d6a596d12985b731cd9a0
4000D <dkdkajej@gmail.com>
2017-08-29 17:10:56 +0900

    added

--- truffle.js
+++ truffle.js
@@ -2,19 +2,35 @@ require("babel-register");
 require("babel-polyfill");
 
 // http://truffleframework.com/tutorials/using-infura-custom-provider
-// const bip39 = require("bip39");
-// const hdkey = require("ethereumjs-wallet/hdkey");
-// const ProviderEngine = require("web3-provider-engine");
-// const WalletSubprovider = require("web3-provider-engine/subproviders/wallet.js");
-// const Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
-// const Web3 = require("web3");
-//
-// // Get our mnemonic and create an hdwallet
+const axios = require("axios");
+const bip39 = require("bip39");
+const hdkey = require("ethereumjs-wallet/hdkey");
+const ProviderEngine = require("web3-provider-engine");
+const WalletSubprovider = require("web3-provider-engine/subproviders/wallet.js");
+const Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
+const Web3 = require("web3");
+
+// Get our mnemonic and create an hdwallet
 // const mnemonic = process.env.MNEMONIC || "price legal churn escape digital timber menu replace crime interest great oblige";
 // const hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
 //
-// const hdPath = "m/44'/60'/0'/0/";
+// const hdPath = "m/44'/60'/0'/1/";
 // const wallet = hdwallet.derivePath(`${ hdPath }0`).getWallet();
+//
+// const makePath = (_path, _n) => Array.from(Array(_n).keys()).map((_, i) => `${ _path }${ i }`);
+// const makeWallet = _path => hdwallet.derivePath(`${ _path }0`).getWallet();
+// const getAddress = _wallet => `0x${ _wallet.getAddress().toString("hex") }`;
+//
+// const paths = makePath(hdPath, 40);
+// const wallets = paths.map(makeWallet);
+// const addresses = wallets.map(getAddress);
+//
+// const faucet = address => axios.get(`http://faucet.ropsten.be:3001/donate/${ address }`)
+//   .then(console.log)
+//   .catch(console.error);
+//
+// addresses.forEach(faucet);
+//
 // const address = `0x${ wallet.getAddress().toString("hex") }`;
 //
 // const providerUrl = "https://ropsten.infura.io";
