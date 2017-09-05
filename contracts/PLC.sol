25789b766e115fb179be5dab5faa7d72b0707ea5
midinpa <midinpa@gmail.com>
2017-09-05 16:07:29 +0900

    token name changed

--- contracts/PLC.sol
+++ contracts/PLC.sol
@@ -5,7 +5,7 @@ import './token/PausableToken.sol';
 import './token/VestedToken.sol';
 
 contract PLC is MintableToken, PausableToken, VestedToken{
-  string public name = "Plus Coin";
+  string public name = "PlusCoin";
   string public symbol = "PLC";
   uint256 public decimals = 18;
 }
