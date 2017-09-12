add9d8639e46e3020bd2562fb3e631c27cda6d26
midinpa <midinpa@gmail.com>
2017-09-12 16:43:25 +0900

    remove short address attack checks

--- contracts/token/BasicToken.sol
+++ contracts/token/BasicToken.sol
@@ -13,19 +13,11 @@ contract BasicToken is SafeMath, ERC20Basic {
   mapping(address => uint256) balances;
 
   /**
-   * @dev Fix for the ERC20 short address attack.
-   */
-  modifier onlyPayloadSize(uint size) {
-    require(msg.data.length >= size + 4);
-     _;
-  }
-
-  /**
   * @dev transfer token for a specified address
   * @param _to The address to transfer to.
   * @param _value The amount to be transferred.
   */
-  function transfer(address _to, uint _value) onlyPayloadSize(2 * 32) returns (bool){
+  function transfer(address _to, uint _value) returns (bool){
     balances[msg.sender] = sub(balances[msg.sender],_value);
     balances[_to] = add(balances[_to],_value);
     Transfer(msg.sender, _to, _value);
